f9ecd7ef04d1c0974b52f9b23dd326c654102d43
razhnoff <shahteryshka@gmail.com>
2020-07-20 00:20:35 +0300

    Add redux, complete task with filters

--- src/App.js
+++ src/App.js
@@ -1,46 +1,88 @@
-import React, { PureComponent } from "react";
+import React, { Component } from "react";
 import { Button } from "./components/Button";
 import { Input } from "./components/Input";
+import { Card } from "./components/Card";
 import { PRIMARY_ACTIVE, PRIMARY } from "./constants";
 import logo from "./assets/Logo.png";
 import "./App.css";
 
-class App extends PureComponent {
+class App extends Component {
   constructor() {
     super();
-    this.init();
-    this.state = {};
+    this.state = {
+      tickets: [],
+      filteredTickets: [],
+      searchId: "",
+      isSortedByPrice: false,
+      isSortedByTime: false,
+      isFilteredByNoTransfers: false,
+      isFilteredByAllTranfers: false,
+      isFilteredByOneTransfer: false,
+      isFilteredByTwoTransfers: false,
+      isFilteredByThreeTransfers: false,
+    };
   }
 
-  init() {
+  initButtonsMeta() {
     this.filtersMeta = [
       {
         key: "all",
         value: "Все",
-        checked: false,
-        onChange: (ev) => {
-          console.log(ev.target.checked);
+        checked: this.state.isFilteredByAllTranfers,
+        onChange: () => {
+          this.setState({
+            isFilteredByAllTranfers: !this.state.isFilteredByAllTranfers,
+            isFilteredByNoTransfers: false,
+          });
         },
       },
       {
         key: "no_transfer",
         value: "Без пересадок",
-        checked: true,
+        checked: this.state.isFilteredByNoTransfers,
+        onChange: () => {
+          this.setState({
+            isFilteredByNoTransfers: !this.state.isFilteredByNoTransfers,
+            isFilteredByAllTranfers: false,
+            isFilteredByOneTransfer: false,
+            isFilteredByTwoTransfers: false,
+            isFilteredByThreeTransfers: false,
+          });
+        },
       },
       {
         key: "one_transfer",
         value: "1 пересадка",
-        checked: true,
+        checked: this.state.isFilteredByOneTransfer,
+        onChange: () => {
+          this.setState({
+            isFilteredByOneTransfer: !this.state.isFilteredByOneTransfer,
+            isFilteredByNoTransfers: false,
+            isFiltredByAllTranfers: false,
+          });
+        },
       },
       {
         key: "two_transfer",
         value: "2 пересадки",
-        checked: true,
+        checked: this.state.isFilteredByTwoTransfers,
+        onChange: () => {
+          this.setState({
+            isFilteredByTwoTransfers: !this.state.isFilteredByTwoTransfers,
+            isFilteredByNoTransfers: false,
+          });
+        },
       },
       {
         key: "three_transfer",
         value: "3 пересадки",
-        checked: false,
+        checked: this.state.isFilteredByThreeTransfers,
+        onChange: () => {
+          this.setState({
+            isFilteredByThreeTransfers: !this.state.isFilteredByThreeTransfers,
+            isFilteredByNoTransfers: false,
+          });
+        },
       },
     ];
   }
@@ -52,6 +94,7 @@ class App extends PureComponent {
       const { searchId } = await response.json();
       this.setState({ searchId });
     } catch (e) {
+      this.fetchSearchId();
       console.error(new Error(e));
     }
   };
@@ -62,10 +105,16 @@ class App extends PureComponent {
   };
 
   fetchAllTickets = async () => {
-    const ticketsUrl = "https://front-test.beta.aviasales.ru/tickets?searchId=";
-    const response = await fetch(ticketsUrl + this.state.searchId);
-    const { tickets } = await response.json();
-    this.setState({ tickets });
+    try {
+      const ticketsUrl =
+        "https://front-test.beta.aviasales.ru/tickets?searchId=";
+      const response = await fetch(ticketsUrl + this.state.searchId);
+      const { tickets } = await response.json();
+      this.setState({ tickets, isLoading: false, filteredTickets: tickets });
+    } catch (e) {
+      this.fetchAllTickets();
+      console.error(new Error(e));
+    }
   };
 
   componentDidMount() {
@@ -73,44 +122,157 @@ class App extends PureComponent {
   }
 
   getFilters = () => {
-    return this.filtersMeta.map(({ value, checked, onChange }, id) => {
-      return <Input key={id} value={value} onChange={onChange} />;
+    return this.filtersMeta.map(({ value, checked, onChange }, key) => {
+      return (
+        <Input key={key} value={value} onChange={onChange} checked={checked} />
+      );
     });
   };
 
+  sortByPrice = (a, b) => {
+    return a.price - b.price;
+  };
+
+  sortByTime = (prevItem, nextItem) => {
+    const prevItemTime = prevItem.segments.reduce((acc, curr) => {
+      return acc + curr.duration;
+    }, 0);
+    const nextItemTime = nextItem.segments.reduce((acc, curr) => {
+      return acc + curr.duration;
+    }, 0);
+    return prevItemTime > nextItemTime ? 1 : -1;
+  };
+
   buttonsMeta = () => {
-    const PRIMATY_ACTIVE_BTN = {
-      type: PRIMARY_ACTIVE,
+    const FILTER_PRICE_BTN = {
+      type: this.state.isSortedByPrice ? PRIMARY_ACTIVE : PRIMARY,
       value: "Самый дешевый",
+      onClick: () => {
+        this.setState({
+          isSortedByPrice: !this.state.isSortedByPrice,
+          isSortedByTime: false,
+        });
+      },
     };
 
-    const PRIMARY_BTN = {
-      type: PRIMARY,
+    const FILTER_TIME_BTN = {
+      type: this.state.isSortedByTime ? PRIMARY_ACTIVE : PRIMARY,
       value: "Самый быстрый",
+      onClick: () => {
+        this.setState({
+          isSortedByTime: !this.state.isSortedByTime,
+          isSortedByPrice: false,
+        });
+      },
     };
 
-    return [PRIMATY_ACTIVE_BTN, PRIMARY_BTN];
+    return [FILTER_PRICE_BTN, FILTER_TIME_BTN];
+  };
+
+  filterByAllTransfers = ({ segments }, key, arr) => {
+    if (!this.state.isFilteredByAllTranfers) {
+      return arr;
+    }
+    const emptyItem = segments.find((item) => item.stops.length === 0);
+    return !emptyItem;
+  };
+
+  filterByNoTransfers = ({ segments }, key, arr) => {
+    if (!this.state.isFilteredByNoTransfers) {
+      return arr;
+    }
+    const stopsLength = segments.reduce((acc, curr) => {
+      return acc + curr.stops.length;
+    }, 0);
+    return stopsLength === 0;
   };
 
   getFilterButtons = () => {
-    return this.buttonsMeta().map(({ type, value }, key) => {
-      return <Button key={key} type={type} value={value} />;
+    return this.buttonsMeta().map(({ type, value, onClick }, key) => {
+      return <Button key={key} type={type} value={value} onClick={onClick} />;
+    });
+  };
+
+  filterByOneTransfers = ({ segments }, key, arr) => {
+    if (!this.state.isFilteredByOneTransfer) {
+      return arr;
+    }
+
+    const stopsLength = segments.reduce((acc, curr) => {
+      return acc + curr.stops.length;
+    }, 0);
+
+    return stopsLength === 1;
+  };
+
+  filterByTwoTransfers = ({ segments }, key, arr) => {
+    if (!this.state.isFilteredByTwoTransfers) {
+      return arr;
+    }
+
+    const stopsLength = segments.reduce((acc, curr) => {
+      return acc + curr.stops.length;
+    }, 0);
+
+    return stopsLength === 2;
+  };
+
+  filterByThreeTransfers = ({ segments }, key, arr) => {
+    if (!this.state.isFilteredByThreeTransfers) {
+      return arr;
+    }
+
+    const stopsLength = segments.reduce((acc, curr) => {
+      return acc + curr.stops.length;
+    }, 0);
+
+    return stopsLength === 3;
+  };
+
+  getCardViews = () => {
+    const filteredTickets = this.state.tickets
+      .filter(this.filterByNoTransfers)
+      .filter(this.filterByAllTransfers)
+      .filter(this.filterByThreeTransfers)
+      .filter(this.filterByTwoTransfers)
+      .filter(this.filterByOneTransfers);
+
+    const sortedByPrice = this.state.isSortedByPrice
+      ? [...filteredTickets].sort(this.sortByPrice)
+      : [...filteredTickets];
+
+    const sortedByTime = this.state.isSortedByTime
+      ? [...sortedByPrice].sort(this.sortByTime)
+      : [...sortedByPrice];
+
+    return sortedByTime.map((ticket, key) => {
+      if (key > 4) {
+        return;
+      }
+
+      return <Card key={key} {...ticket} />;
     });
   };
 
   render() {
-    console.log(this.state);
+    this.initButtonsMeta();
+
     return (
       <div className="App">
-        <a href={"#"} className={"logo"} title={"logo"}>
-          <img src={logo} alt={"logo"} />
-        </a>
+        <div className className={"logo"}>
+          <a href={"#"} title={"logo"}>
+            <img src={logo} alt={"logo"} />
+          </a>
+        </div>
         <div className={"mainWrapper"}>
           <div className={"leftside"}>
             <h1 className={"title"}>Количество пересадок</h1>
             {this.getFilters()}
           </div>
-          <div className={"wrapper"}>{this.getFilterButtons()}</div>
+          <div className={"wrapper"}>
+            {this.getFilterButtons()}
+            {this.getCardViews()}
+          </div>
         </div>
       </div>
     );
---
+++ src/components/Card/Card.js
@@ -0,0 +1,118 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import companyLogo from "../../assets/company.png";
+import "./Card.css";
+
+export class Card extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  getTimeByKey = (timestamp, key) => {
+    const options = {
+      minutes: new Date(timestamp).getMinutes(),
+      hours: new Date(timestamp).getHours(),
+    };
+
+    const value = options[key];
+
+    if (value < 10) {
+      return `0${value}`;
+    }
+
+    return value;
+  };
+
+  getDuration = (duration) => {
+    const hours = Math.floor(duration / 60);
+    const minutes = duration % 60;
+
+    return `${hours}ч ${minutes}м`;
+  };
+
+  getAllTime = (timestamp) => {
+    return `${this.getTimeByKey(timestamp, "hours")}:${this.getTimeByKey(
+      timestamp,
+      "minutes"
+    )}`;
+  };
+
+  getDestinationTime = ({ date, duration }) => {
+    const originTimeStamp = Date.parse(date);
+    const durationMs = duration * 60 * 1000;
+
+    return originTimeStamp + durationMs;
+  };
+
+  getCardRows = ({ segments }) => {
+    return segments.map(
+      ({ date, destination, duration, origin, stops }, key) => {
+        const destinationTime = this.getDestinationTime({ date, duration });
+
+        return (
+          <div key={key} className={"row"}>
+            <div className={"row_titles"}>
+              <span className={"col_title"}>
+                {origin} – {destination}
+              </span>
+              <span className={"col_title"}>В пути</span>
+              <span className={"col_title"}>
+                {this.getTransfersTitle(stops)}
+              </span>
+            </div>
+            <div className={"row_data"}>
+              <span className={"col_data"}>
+                {this.getAllTime(date)} – {this.getAllTime(destinationTime)}
+              </span>
+              <span className={"col_data"}>{this.getDuration(duration)}</span>
+              <span className={"col_data"}>{this.getTransfersData(stops)}</span>
+            </div>
+          </div>
+        );
+      }
+    );
+  };
+
+  getTransfersData = (transfers) => {
+    if (!transfers.length) {
+      return "–";
+    }
+
+    return transfers.join(", ");
+  };
+
+  getTransfersTitle = (transfers) => {
+    if (!transfers.length) {
+      return "0 пересадок";
+    } else if (transfers.length === 1) {
+      return "1 пересадка";
+    }
+
+    return `${transfers.length} пересадки`;
+  };
+
+  render() {
+    const { price, carrier: code, segments } = this.props;
+    
+    return (
+      <div className={"card_container"}>
+        <div className={"main_info"}>
+          <span className={"price"}>{price} P</span>
+          <div className={"company_logo"}>
+            <img
+              src={`https://pics.avs.io/99/36/${code}.png`}
+              alt={"company logo"}
+            />
+          </div>
+        </div>
+        <div className={"additional_info"}>
+          {this.getCardRows({ segments })}
+        </div>
+      </div>
+    );
+  }
+}
+
+Card.propTypes = {
+  price: PropTypes.number,
+};
