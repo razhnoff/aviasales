67fdf009186b83cef797e862cf203fb1530b00f9
razhnoff <shahteryshka@gmail.com>
2021-04-07 01:43:38 +0300

    Updates

---
+++ precache-manifest.12cceb03ff95a12dd2ecd6a8636ee277.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ca95c9e2db22ba99fde7929916c8893",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac3831e5ba630f8e0258",
+    "url": "/static/css/main.505b6321.chunk.css"
+  },
+  {
+    "revision": "2b4a8a32921c3a435bed",
+    "url": "/static/js/2.be3e2a53.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.be3e2a53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac3831e5ba630f8e0258",
+    "url": "/static/js/main.5592c5de.chunk.js"
+  },
+  {
+    "revision": "cb8e000bf916c88e000c",
+    "url": "/static/js/runtime-main.b2fa1274.js"
+  }
+]);
